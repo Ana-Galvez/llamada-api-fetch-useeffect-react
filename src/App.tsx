e214b8319c8d02f7e04a2c134e7cc131c0f92083
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-14 02:54:53 -0300

    feat:manejo errores

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,30 @@ interface Todo{
 }
 const App = () => {
   const [todo,setTodo]=useState<null | Todo> (null)
+  const [error, setError] = useState<null | string>(null)
   useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response=>response.json())
+      .then(response=>{
+        if (response.ok) return response.json()
+        setError("Hubo un error al cargar los datos")
+      })
       .then((data:Todo)=>{
         console.log(data)
         setTodo(data)
       })
-      .catch(error=>console.log(error))
+      .catch(error=>console.log(error)
+       )
+      
   },[])
 
-  if (!todo) return <p>Cargando datos....</p>
+  if (error) return <p className="text-center text-danger">{error}</p>
+  if (!todo) return <p className="text-center">Cargando datos....</p>
+
   return (
     <div className="container mx-auto">
       <h1>Obtención de API con fetch</h1>
-      <h2>{todo.title}</h2>
+      <h2>Título de la tarea: {todo.title}</h2>
+      <h2 className={todo.completed ? 'text-success' : 'text-danger'}>Estado de la tarea: {todo.completed ? "Tarea completada" : "Tarea NO completada"}</h2>
     </div>
   )
 }
