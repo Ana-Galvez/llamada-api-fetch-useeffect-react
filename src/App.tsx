b053f1c29f94491cdb2acee8702597e3acca9e86
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-14 02:14:35 -0300

    feat: parte 1 fetch then

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react"
+
+interface Todo{
+  userId:number;
+  id:number;
+  title:string;
+  completed:boolean;
+}
 const App = () => {
+  const [todo,setTodo]=useState<Todo | null> (null)
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response=>response.json())
+      .then((data:Todo)=>{
+        console.log(data)
+        setTodo(data)
+      })
+      .catch(error=>console.log(error))
+  },[])
   return (
-    <div>App</div>
+    <div className="container mx-auto">
+      <h1>Obtención de API con fetch</h1>
+      <h2>{todo?.title}</h2>
+    </div>
   )
 }
 export default App
